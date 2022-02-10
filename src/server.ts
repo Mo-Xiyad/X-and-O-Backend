a819cb85b15810ef75a892fe1e6bfc408231385c
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2022-02-10 23:40:56 +0100

    formetting

--- src/server.ts
+++ src/server.ts
@@ -1,79 +1,101 @@
 import http from "http";
 import { app } from "./app";
-import { Server} from "socket.io"
-import { shared } from "shared";
+import { Server, Socket } from "socket.io"
+import { shared } from "./shared";
+import { checkWinner } from "./utils/checkWinner";
+import { isGameOver } from "./utils/isGameOver";
 
 const httpServer = http.createServer(app)
 
-const io = new Server(httpServer);
+const io = new Server(httpServer, { cors: { origin: "https://strive-tic-tac-toe.herokuapp.com" } });
 
 io.on("connection", (socket) => {
- socket.on(
-   "loggedIn",
-   ({ name, symbol }: { name: string; symbol: "X" | "O" }) => {
-     console.log(name + " is waiting for a game...");
+    console.log("New client connected with ID " + socket.id);
 
-     const onDisconnect = () => {
-       console.log("Client " + socket.id + " disconnected");
-       try {
-         shared.waitingList[symbol] = shared.waitingList[symbol].filter(
-           (u) => u.socket.id !== socket.id
-         );
-       } catch (error) {
-         console.log({ symbol });
-         console.error(error);
-       }
-     };
+    socket.on("loggedIn", ({ name, symbol }: { name: string, symbol: "X" | "O" }) => {
+        console.log(name + " is waiting for a game...")
 
-     socket.on("disconnect", onDisconnect);
+        const onDisconnect = () => {
+            console.log("Client " + socket.id + " disconnected");
+            try {
+                shared.waitingList[symbol] = shared.waitingList[symbol].filter(u => u.socket.id !== socket.id)
+            } catch (error) {
+                console.log({ symbol })
+                console.error(error)
+            }
+        }
 
-     socket.on("leave", () => {
-       console.log("Client " + socket.id + " left");
-       try {
-         shared.waitingList[symbol] = shared.waitingList[symbol].filter(
-           (u) => u.socket.id !== socket.id
-         );
-       } catch (error) {
-         console.log({ symbol });
-         console.error(error);
-       }
+        socket.on("disconnect", onDisconnect);
 
-       socket.off("disconnect", onDisconnect);
-     });
+        socket.on("leave", () => {
+            console.log("Client " + socket.id + " left");
+            try {
+                shared.waitingList[symbol] = shared.waitingList[symbol].filter(u => u.socket.id !== socket.id)
+            } catch (error) {
+                console.log({ symbol })
+                console.error(error)
+            }
 
-     const opponentSymbol = symbol === "X" ? "O" : "X";
+            socket.off("disconnect", onDisconnect);
+        });
 
-     console.log({ opponentSymbol });
+        const opponentSymbol = symbol === "X" ? "O" : "X";
 
-     if (shared.waitingList[opponentSymbol].length === 0) {
-       socket.emit("waitingForOpponent");
-       try {
-         shared.waitingList[symbol].push({ name, socket });
-       } catch (error) {
-         console.log({ symbol });
-         console.error(error);
-       }
-     } else {
-       const opponent = shared.waitingList[opponentSymbol].shift()!;
+        console.log({ opponentSymbol })
 
-       opponent.socket.emit("gameStarted", {
-         opponent: {
-           name: name,
-           socketId: socket.id,
-         },
-         symbol: opponentSymbol,
-       });
+        if (shared.waitingList[opponentSymbol].length === 0) {
+            socket.emit("waitingForOpponent")
+            try {
+                shared.waitingList[symbol].push({ name, socket })
+            } catch (error) {
+                console.log({ symbol })
+                console.error(error)
+            }
+        } else {
+            const opponent = shared.waitingList[opponentSymbol].shift()!
 
-       socket.emit("gameStarted", {
-         opponent: {
-           name: opponent.name,
-           socketId: opponent.socket.id,
-         },
-         symbol: symbol,
-       });
-     }
-   }
- );
+            opponent.socket.emit("gameStarted", {
+                opponent: {
+                    name: name,
+                    socketId: socket.id
+                },
+                symbol: opponentSymbol,
+            })
+
+            socket.emit("gameStarted", {
+                opponent: {
+                    name: opponent.name,
+                    socketId: opponent.socket.id
+                },
+                symbol: symbol
+            })
+        }
+    })
+
+    socket.on("matrixUpdate", ({ matrix, opponent }) => {
+        console.log({ matrix }, opponent)
+
+        // check if the game is over
+        const winner = checkWinner(matrix)
+
+        if (!winner) {
+            const gameIsOver = isGameOver(matrix)
+
+            if (gameIsOver) {
+                // this is a draw
+                socket.emit("gameOver", { winner, matrix })
+                socket.to(opponent.socketId).emit("gameOver", { winner, matrix })
+            } else {
+                // the game is not over yet
+                socket.emit("waitingForMove")
+                socket.to(opponent.socketId).emit("yourTurn", { matrix })
+            }
+        } else {
+            socket.emit("gameOver", { winner, matrix })
+            socket.to(opponent.socketId).emit("gameOver", { winner, matrix })
+        }
+
+    })
 
 
 })
